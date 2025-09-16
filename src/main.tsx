3bb46b6dfa7984e1f7f51deab998996410928edf
cheerzhang <zhanglenus@gmail.com>
2025-09-16 22:13:17 +0200

    add new picture

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,11 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+// remove caches
+if ("caches" in window) {
+  caches.keys().then(function (names) {
+    for (let name of names) caches.delete(name);
+  });
+}
+
 createRoot(document.getElementById("root")!).render(<App />);
